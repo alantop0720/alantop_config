4644263fdbdac96ffbdcaff0ad818fca669c9bd8
alantop0720 <30796007@qq.com>
2026-05-28 06:55:35 +0800

    build: add application window icon and resource configuration
    add my.ico icon file, create qrc resource config, link resource in cmake, set window icon for app

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "qconfigwidget.h"
 
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon(":/my.ico"));
     QConfigWidget w;
     w.showMaximized();
     return QApplication::exec();
